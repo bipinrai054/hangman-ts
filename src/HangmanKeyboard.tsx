d3ea7bb2f676b9054cb8863f6311387662c87e9a
bipnrai054 <bipinrai054@gmail.com>
2022-11-13 17:43:06 +0545

    word done

--- src/HangmanKeyboard.tsx
+++ src/HangmanKeyboard.tsx
@@ -1,5 +1,32 @@
-import React from 'react';
-
 export default function HangmanKeyboard() {
-  return <div>HangmanKeyboard</div>;
+  const word = 'test';
+  const guessedLetters = ['t'];
+  return (
+    <div
+      style={{
+        display: 'flex',
+        gap: '.25rem',
+        fontSize: '6rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontFamily: 'monospace',
+      }}
+    >
+      {word.split('').map((letter, index) => {
+        return (
+          <span style={{ borderBottom: '.1em solid black' }} key={index}>
+            <span
+              style={{
+                visibility: guessedLetters.includes(letter)
+                  ? 'visible'
+                  : 'hidden',
+              }}
+            >
+              {letter}
+            </span>
+          </span>
+        );
+      })}
+    </div>
+  );
 }
